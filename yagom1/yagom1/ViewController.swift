3de6e56416b15bcd3757d3714bd7d8319cec2885
lkhwan0204 <lkhwan0204@gmail.com>
2025-02-24 23:36:39 +0900

    part4

---
+++ yagom1/yagom1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yagom1
+//
+//  Created by kyuhyeon Lee on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ yagom1/yagom1Tests/yagom1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  yagom1Tests.swift
+//  yagom1Tests
+//
+//  Created by kyuhyeon Lee on 2/24/25.
+//
+
+import Testing
+@testable import yagom1
+
+struct yagom1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
